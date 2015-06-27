f02c0c86273a78454fea6b197bea0c6bd1848e03
dizzledash <sueren.glinka@web.de>
2015-06-27 10:17:16 +0200

    Saving is now working, but not yet in the way we wanted it.

---
+++ app/src/main/java/com/example/zwiesel/recipe_app/Recipe.java
@@ -0,0 +1,60 @@
+package com.example.zwiesel.recipe_app;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sören on 24.06.2015.
+ */
+public class Recipe {
+
+    static final int CATEGORY_APPETIZER = 0;
+    static final int CATEGORY_ENTREE = 1;
+    static final int CATEGORY_DESSERT = 2;
+
+    private int category;
+    private String name, description;
+    private ArrayList<String[]> aList = new ArrayList<>();
+
+    public Recipe(String n){
+        name = n;
+    }
+
+    public void addIngredient(String title, String amount, String unit){
+        String[] ingredient = new String[]{title, amount, unit};
+        aList.add(ingredient);
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setDescription(String d){
+        description = d;
+    }
+
+    public void setCategory(int c){
+        category = c;
+    }
+
+    public void setIngredient(int index, String title, String amount, String unit){
+        String[] ingredient = new String[]{title, amount, unit};
+        aList.remove(index);
+        aList.add(index, ingredient);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getCategory(){
+        return category;
+    }
+
+    public String[] getIngredient(int index){
+        return aList.get(index);
+    }
+}
